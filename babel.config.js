7755b0bba2e5699d4ae71161c91afe60ba7f470c
dovexiao <2867179160@qq.com>
2025-08-06 21:06:57 +0800

    feat -留底

--- babel.config.js
+++ babel.config.js
@@ -1,29 +1,29 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    [
-      'react-native-reanimated/plugin',
+    presets: ['module:@react-native/babel-preset'],
+    plugins: [
+        [
+            'react-native-reanimated/plugin',
+        ],
+        [
+            'module-resolver',
+            {
+                root: ['./src'],
+                extensions: ['.js', '.jsx', '.ts', '.tsx'],
+                alias: {
+                    '@contexts': './src/contexts',
+                    '@global': './src/global',
+                    '@components': './src/components',
+                    '@assets': './src/assets',
+                    '@utils': './src/utils',
+                    '@screens': './src/screens',
+                    '@services': './src/services',
+                    '@hooks': './src/hooks',
+                    '@stores': './src/stores',
+                    '@types': './src/types',
+                    '@navigation': './src/navigation',
+                    '@': './src',
+                },
+            },
+        ],
     ],
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        alias: {
-          '@contexts': './src/contexts',
-          '@global': './src/global',
-          '@components': './src/components',
-          '@assets': './src/assets',
-          '@utils': './src/utils',
-          '@screens': './src/screens',
-          '@services': './src/services',
-          '@hooks': './src/hooks',
-          '@stores': './src/stores',
-          '@types': './src/types',
-          '@navigation': './src/navigation',
-          '@': './src',
-        },
-      },
-    ],
-  ],
 };
