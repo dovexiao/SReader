afa568b681b94f990996f38fb9cafaecf0610fed
dovexiao <2867179160@qq.com>
2025-08-10 00:02:55 +0800

    feat -新增笔记回收站组件

---
+++ src/center/recycleBin/utils/generateNoteRecycleBin.ts
@@ -0,0 +1,31 @@
+// 生成示例数据
+import { NoteSection } from '@/center/recycleBin/types';
+import { ALPHABET } from '@/center/recycleBin/components/AlphabetNavigator.tsx';
+import { SectionListData } from 'react-native';
+
+export const generateNoteRecycleBin = (): SectionListData<NoteSection>[] => {
+    const sectionListData: SectionListData<NoteSection>[] = [];
+
+    ALPHABET.forEach(letter => {
+        const noteSections: NoteSection[] = [];
+        const noteCount: number = Math.floor(Math.random() * 5);
+
+        for (let i = 1; i <= noteCount; i++) {
+            noteSections.push({
+                noteId: `${letter}${i}`,
+                title: `${letter}笔记 ${i}`,
+                introduce: 'xxxxx-xxxxx-xxxxx-xxxxx-xxxxx',
+                createdAt: new Date().toISOString(),
+            });
+        }
+
+        if (noteSections.length > 0) {
+            sectionListData.push({
+                title: letter,
+                data: noteSections,
+            });
+        }
+    });
+
+    return sectionListData;
+};
